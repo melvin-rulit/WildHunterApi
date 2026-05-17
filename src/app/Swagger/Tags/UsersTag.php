c6a2dea2110e999967a2b554675af876f0c3a0b1
melvin <melvin.melvin.rulit@gmail.com>
2026-05-17 11:10:14 +0000

    swagger тагс для пользователей

---
+++ src/app/Swagger/Tags/UsersTag.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Swagger\Tags;
+
+namespace App\Swagger\Tags;
+
+use OpenApi\Attributes as OA;
+
+#[OA\Tag(
+    name: "Users",
+    description: "Работа с пользователями"
+)]
+
+class UsersTag
+{
+
+}
