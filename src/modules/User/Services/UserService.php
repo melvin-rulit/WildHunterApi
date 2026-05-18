04ded18b1f69eba9ee57e40a910044537e2ab216
melvin <melvin.melvin.rulit@gmail.com>
2026-05-18 07:45:16 +0000

    сервис user

--- src/modules/User/Services/UserService.php
+++ src/modules/User/Services/UserService.php
@@ -6,11 +6,15 @@ use App\Models\User;
 
 class UserService
 {
-    public function searchUserById(string $id)
+    public function searchById(string $id): ?User
     {
-        return User::where('id', $id)->first();
+        return User::find($id);
     }
-    public function searchUserByQuery(string $query)
+    public function findByEmail(string $email): ?User
+    {
+        return User::firstWhere('email', $email);
+    }
+    public function searchByQuery(string $query)
     {
         return  User::where(function ($q) use ($query) {
         $q->where('user_name', 'LIKE', $query.'%')
