8ddefdec9f1088430ee6e95f31773cd0b84d95d3
melvin <melvin.melvin.rulit@gmail.com>
2026-05-18 07:43:38 +0000

    маршрут логина

--- src/modules/User/Routes/api/v1/api.php
+++ src/modules/User/Routes/api/v1/api.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Modules\User\Controllers\Api\BookRest\AuthController;
 use Modules\User\Controllers\Api\BookRest\UserController;
+
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
      Route::get('/users', [UserController::class, 'searchUser']);
