8ede70ccd08c5e7856a77d2527e3ec2abdf3b7c9
melvin <melvin.melvin.rulit@gmail.com>
2026-05-18 11:29:55 +0000

    сервис авторизации....

---
+++ src/modules/User/Services/Auth/AuthService.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Modules\User\Services\Auth;
+
+use Illuminate\Support\Facades\Hash;
+use Modules\User\Dto\Auth\LoginData;
+use Modules\User\Services\UserService;
+use App\Exceptions\UnauthorizedException;
+
+class AuthService
+{
+    public function __construct(private UserService $userService)
+    {
+    }
+
+    /**
+     * @throws UnauthorizedException
+     */
+    public function login(LoginData $dto): array
+    {
+        $user = $this->userService->findByEmail($dto->email);
+
+        if (! $user || ! Hash::check($dto->password, $user->password)) {
+            throw new UnauthorizedException();
+        }
+
+        $token = $user->createToken('api_token')->plainTextToken;
+
+        return [
+            'user' => $user,
+            'token' => $token,
+        ];
+    }
+}
