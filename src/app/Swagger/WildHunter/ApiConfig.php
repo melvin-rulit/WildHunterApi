fb79b108434fb1a96fdb1c058faf23e0eaa2f60c
melvin <melvin.melvin.rulit@gmail.com>
2026-05-18 10:30:41 +0000

    убрал default

---
+++ src/app/Swagger/WildHunter/ApiConfig.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Swagger\WildHunter;
+
+class ApiConfig
+{
+    public const string VERSION = 'v1';
+}
--- src/config/l5-swagger.php
+++ src/config/l5-swagger.php
@@ -1,53 +1,8 @@
 <?php
 
 return [
-    'default' => 'bookrest',
+    'default' => 'hunter',
     'documentations' => [
-        'default' => [
-            'api' => [
-                'title' => 'L5 Swagger UI',
-            ],
-
-            'routes' => [
-                /*
-                 * Route for accessing api documentation interface
-                 */
-                'api' => 'api/documentation',
-            ],
-            'paths' => [
-                /*
-                 * Edit to include full URL in ui for assets
-                 */
-                'use_absolute_path' => env('L5_SWAGGER_USE_ABSOLUTE_PATH', true),
-
-                /*
-                * Edit to set path where swagger ui assets should be stored
-                */
-                'swagger_ui_assets_path' => env('L5_SWAGGER_UI_ASSETS_PATH', 'vendor/swagger-api/swagger-ui/dist/'),
-
-                /*
-                 * File name of the generated json documentation file
-                 */
-                'docs_json' => 'api-docs.json',
-
-                /*
-                 * File name of the generated YAML documentation file
-                 */
-                'docs_yaml' => 'api-docs.yaml',
-
-                /*
-                 * Set this to `json` or `yaml` to determine which documentation file to use in UI
-                 */
-                'format_to_use_for_docs' => env('L5_FORMAT_TO_USE_FOR_DOCS', 'json'),
-
-                /*
-                 * Absolute paths to directory containing the swagger annotations are stored.
-                 */
-                'annotations' => [
-                    base_path('app/Swagger'),
-                ],
-            ],
-        ],
         'bookrest' => [
             'api' => [
                 'title' => 'Bookrest API',
@@ -61,6 +16,21 @@ return [
                     base_path('app/Swagger/Bookrest'),
                 ],
              'docs_json' => 'bookrest-api-docs.json',
+            ],
+        ],
+        'hunter' => [
+            'api' => [
+                'title' => 'WildHunter API',
+            ],
+            'routes' => [
+                'api' => 'api/wild_hunter/documentation',
+                'docs' => 'docs/wild_hunter',
+            ],
+            'paths' => [
+                'annotations' => [
+                    base_path('app/Swagger/WildHunter'),
+                ],
+                'docs_json' => 'wild_hunter-api-docs.json',
             ],
         ],
     ],
