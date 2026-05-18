4f00858e0e47c73afe96135d834efa0ae2ec829f
melvin <melvin.melvin.rulit@gmail.com>
2026-05-18 07:44:34 +0000

    валидация логина

---
+++ src/modules/User/Http/Requests/Auth/LoginRequest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\User\Http\Requests\Auth;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LoginRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'email' => ['required', 'email', 'max:255'],
+            'password' => ['required', 'string', 'min:6'],
+        ];
+    }
+}
