c3bbd8d8cc02dfb240d186325ba64cbf5907b3e2
melvin <melvin.melvin.rulit@gmail.com>
2026-05-18 15:10:30 +0000

    trace_id  к всем запросам api

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Exceptions\BaseException;
+use App\Http\Middleware\AttachTraceId;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -13,7 +14,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->api(
+        [
+            AttachTraceId::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->render(function (\Throwable $e, $request) {
