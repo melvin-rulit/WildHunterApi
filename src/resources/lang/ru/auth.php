940363ebc24d32487b114437eefeb6c094d32ce2
melvin <melvin.melvin.rulit@gmail.com>
2026-05-21 11:37:20 +0000

    перевод для регистрации

--- src/resources/lang/ru/auth.php
+++ src/resources/lang/ru/auth.php
@@ -8,6 +8,28 @@ return [
     'rules' => [
         'registration_disabled' => 'Регистрация отключена',
     ],
+    'validation' => [
+        'first_name_required' => 'Имя обязательно',
+        'last_name_required'  => 'Фамилия обязательна',
+
+        'role_required' => 'Роль обязательна',
+        'role_invalid'  => 'Недопустимая роль',
+
+        'phone_required' => 'Телефон обязателен',
+        'phone_unique'   => 'Такой телефон уже существует',
+
+        'email_required' => 'Email обязателен',
+        'email_invalid'  => 'Неверный формат email',
+        'email_unique'   => 'Этот email уже занят',
+
+        'password_required' => 'Пароль обязателен',
+        'password_min'      => 'Пароль должен быть минимум 8 символов',
+        'password_upper_lower' => 'Пароль должен содержать заглавные и строчные буквы',
+        'password_number' => 'Пароль должен содержать хотя бы одну цифру',
+
+        'term_required'  => 'Необходимо согласие с условиями',
+        'term_accepted'  => 'Необходимо принять условия',
+    ],
     'successes' => [
 
     ]
