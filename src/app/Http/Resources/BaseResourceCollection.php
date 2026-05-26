e26cbe82ac1f0cb4471a5ed0954fcd441e879d17
melvin <melvin.melvin.rulit@gmail.com>
2026-05-26 08:20:08 +0000

    базовые файлы resource с методом needs

---
+++ src/app/Http/Resources/BaseResourceCollection.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+
+class BaseResourceCollection extends AnonymousResourceCollection
+{
+    public $needs = [];
+
+    public function toArray($request)
+    {
+        $this->collection->map(function($item){
+           $item->needs = $this->needs;
+        });
+
+        return $this->collection->map->toArray($request)->all();
+    }
+}
