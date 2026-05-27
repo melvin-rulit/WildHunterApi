07fd7b599ed01e5a7b1b9b7681be8a16f07bada5
melvin <melvin.melvin.rulit@gmail.com>
2026-05-27 08:49:54 +0000

    файл перевода для обновления пароля

---
+++ src/resources/lang/ru/password.php
@@ -0,0 +1,20 @@
+<?php
+return [
+    'errors' => [
+        'current_password_incorrect' => 'Текущий пароль неверный',
+    ],
+    'rules' => [
+
+    ],
+    'validation' => [
+        'current_password_required' => 'Текущий пароль обязателен',
+        'new_password_required'  => 'Новый пароль обязателен',
+        'new_password_confirmed'  => 'Подтверждение пароля не совпадает',
+        'new_password_different'  => 'Новый пароль должен отличаться от текущего',
+        'confirmation_password_required'  => 'Подтверждение пароля обязательно',
+
+    ],
+    'successes' => [
+        'password_update_successfully' => 'Пароль успешно обновлен',
+    ]
+];
