3cb8bda5f71170133c3b4cae49535b861f3ff6ff
melvin <melvin.melvin.rulit@gmail.com>
2026-06-02 12:20:22 +0300

    описание эндпоинта на подписку рассылки

--- src/app/Swagger/Paths/UserPath.php
+++ src/app/Swagger/Paths/UserPath.php
@@ -115,4 +115,55 @@ class UserPath
     )]
     public function changePassword(): void
     {}
+
+    #[OA\Post(
+        path: "/api/" . ApiConfig::VERSION . "/user/newsletter/subscribe",
+        summary: "Подписка на рассылку",
+        security: [['bearerAuth' => []]],
+        requestBody: new OA\RequestBody(
+            required: true,
+            content: new OA\JsonContent(
+                required: ["email", "privacy_policy"],
+                properties: [
+                    new OA\Property(
+                        property: "email",
+                        type: "string",
+                        format: "email",
+                        example: "user@example.com"
+                    ),
+                    new OA\Property(
+                        property: "privacy_policy",
+                        type: "boolean",
+                        example: true
+                    ),
+                ]
+            )
+        ),
+        tags: ["Users"],
+        responses: [
+            new OA\Response(
+                ref: "#/components/responses/SuccessResponse",
+                response: 200,
+            ),
+            new OA\Response(
+                ref: "#/components/responses/ValidationError",
+                response: 422
+            ),
+            new OA\Response(
+                response: 401,
+                description: "Не авторизован",
+                content: new OA\JsonContent(
+                    properties: [
+                        new OA\Property(
+                            property: "message",
+                            type: "string",
+                            example: "Не авторизован"
+                        )
+                    ]
+                )
+            )
+        ]
+    )]
+    public function subscribeNewsletter(): void
+    {}
 }
