b358b252b6894607067df8a055f1819aa36c3078
melvin <melvin.melvin.rulit@gmail.com>
2026-06-03 15:27:35 +0300

    модель Отелей

---
+++ src/modules/Hotel/Models/Hotel.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Modules\Hotel\Models;
+
+use App\Observers\HotelObserver;
+use Modules\Booking\Models\Bookable;
+use Modules\Booking\Traits\CapturesService;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Notifications\Notifiable;
+
+class Hotel extends Bookable
+{
+    use SoftDeletes;
+    use Notifiable;
+    use CapturesService;
+    protected $table                              = 'bc_hotels';
+    public    $type                               = 'hotel';
+    protected $fillable      = [
+        'title',
+        'content',
+        'status',
+    ];
+    protected string $slugField     = 'slug';
+    protected string $slugFromField = 'title';
+    protected string $seo_type      = 'hotel';
+    protected $casts = [
+        'policy' => 'array',
+        'extra_price' => 'array',
+        'service_fee' => 'array',
+        'surrounding' => 'array',
+    ];
+
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+    }
+
+    protected static function booted(): void
+    {
+        static::observe(HotelObserver::class);
+    }
+
+    public static function getModelName()
+    {
+        return __("hotel.name.model_name");
+    }
+}
