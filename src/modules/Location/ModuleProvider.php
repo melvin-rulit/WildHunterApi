8ec26f12d84d76a0fa5544644f248de93cab0053
melvin <melvin.melvin.rulit@gmail.com>
2026-06-03 11:33:02 +0300

    провайдеры модуля Локации

---
+++ src/modules/Location/ModuleProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Modules\Location;
+
+use Modules\ModuleServiceProvider;
+use Modules\Location\Models\Location;
+use Modules\Core\Helpers\SitemapHelper;
+
+class ModuleProvider extends ModuleServiceProvider
+{
+
+    public function boot(SitemapHelper $sitemapHelper): void
+    {
+        $this->loadMigrationsFrom(__DIR__ . '/Database/Migrations');
+
+        if (is_installed()) {
+            $sitemapHelper->add("location", [app()->make(Location::class), 'getForSitemap']);
+        }
+    }
+    /**
+     * Register bindings in the container.
+     *
+     * @return void
+     */
+    public function register(): void
+    {
+        $this->app->register(RouterServiceProvider::class);
+    }
+
+
+    public static function getAdminMenu()
+    {
+
+    }
+}
