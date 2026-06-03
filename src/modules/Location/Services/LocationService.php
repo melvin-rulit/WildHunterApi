590d290f96cf7230f74ddd169573487b4a8b4b0f
melvin <melvin.melvin.rulit@gmail.com>
2026-06-03 13:08:06 +0300

    сервис для локаций

---
+++ src/modules/Location/Services/LocationService.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Modules\Location\Services;
+
+use Modules\Location\Models\Location;
+use Modules\Location\Dto\LocationFilterData;
+
+class LocationService
+{
+    public function getLocations(LocationFilterData $dto): array
+    {
+        $locations = Location::published()
+            ->when($dto->order_by, function ($q) use ($dto) {
+                $q->orderBy($dto->order_by, $dto->order_direction ?? 'asc');
+            })
+            ->when($dto->limit, fn($q) => $q->limit($dto->limit))
+            ->get();
+
+        return [
+            'code' => '',
+            'data' => $locations
+        ];
+    }
+}
