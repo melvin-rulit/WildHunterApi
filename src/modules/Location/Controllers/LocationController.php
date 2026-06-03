941ab7ef3281177484ffe74239669c166988ddeb
melvin <melvin.melvin.rulit@gmail.com>
2026-06-03 12:45:59 +0300

    контроллер для локаций

---
+++ src/modules/Location/Controllers/LocationController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Modules\Location\Controllers;
+
+use Illuminate\Http\JsonResponse;
+use App\Http\Controllers\Controller;
+use App\Http\Responses\SuccessResponse;
+use Modules\Location\Dto\LocationFilterData;
+use Modules\Location\Http\Request\LocationFilterRequest;
+use Modules\Location\Http\Resources\LocationResource;
+use Modules\Location\Services\LocationService;
+
+
+class LocationController extends Controller
+{
+    public function __construct(protected LocationService $locationService)
+    {
+    }
+    public function getLocations(LocationFilterRequest $request): JsonResponse
+    {
+        $dto = LocationFilterData::fromRequest($request);
+        $result = $this->locationService->getLocations($dto);
+
+        return new SuccessResponse(
+            data: LocationResource::collection($result['data'])
+        );
+    }
+}
