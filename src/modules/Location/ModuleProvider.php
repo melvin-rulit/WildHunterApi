dcc002c8552083f179d905e043c34da057ee2eb9
melvin <melvin.melvin.rulit@gmail.com>
2026-06-03 13:59:01 +0300

    файл конфига модуля локаций и загрузка файла

--- src/modules/Location/ModuleProvider.php
+++ src/modules/Location/ModuleProvider.php
@@ -12,6 +12,7 @@ class ModuleProvider extends ModuleServiceProvider
     public function boot(SitemapHelper $sitemapHelper): void
     {
         $this->loadMigrationsFrom(__DIR__ . '/Database/Migrations');
+        $this->mergeConfigFrom(__DIR__ . '/Configs/config.php', 'role');
 
         if (is_installed()) {
             $sitemapHelper->add("location", [app()->make(Location::class), 'getForSitemap']);
