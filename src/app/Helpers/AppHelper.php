cce1848d08e09fa97339cc100f1f70bfae14ac69
melvin <melvin.melvin.rulit@gmail.com>
2026-06-03 12:32:30 +0300

    хэлпер для всего приложения

--- src/app/Helpers/AppHelper.php
+++ src/app/Helpers/AppHelper.php
@@ -1,1 +1,18 @@
 <?php
+
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Cache;
+
+
+function is_enable_registration(): bool
+{
+    return !setting_item('user_disable_register');
+}
+
+function is_installed(): bool
+{
+    return file_exists(storage_path('installed'));
+}
+
+
+
