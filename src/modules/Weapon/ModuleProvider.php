aa389d76c7c47d7893cb85a39dbd1b859d5b3a67
melvin <melvin.melvin.rulit@gmail.com>
2026-06-10 11:26:43 +0300

    главные файлы для модуля оружие

---
+++ src/modules/Weapon/ModuleProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Modules\Weapon;
+
+use Modules\Weapon\Models\WeaponType;
+use Modules\Core\Helpers\SitemapHelper;
+use Modules\ModuleServiceProvider;
+
+class ModuleProvider extends ModuleServiceProvider
+{
+
+    public function boot(SitemapHelper $sitemapHelper): void
+    {
+        $this->loadMigrationsFrom(__DIR__ . '/Migrations');
+
+        if(is_installed() and WeaponType::isEnable()){
+
+            $sitemapHelper->add("weapon",[app()->make(WeaponType::class),'getForSitemap']);
+        }
+    }
+    /**
+     * Register bindings in the container.
+     *
+     * @return void
+     */
+    public function register(): void
+    {
+        $this->app->register(RouterServiceProvider::class);
+    }
+}
