f76a842b26a143ecd0476f716945909e4982a9b9
melvin <melvin.melvin.rulit@gmail.com>
2026-06-10 10:49:49 +0300

    добавил вывод роли пользователя

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -6,10 +6,13 @@ namespace App\Models;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Modules\Role\Models\Role;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
@@ -29,5 +32,19 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+
+    protected function roleName():Attribute{
+        return Attribute::make(
+            get:function(){
+                return $this->role->name ?? '';
+            }
+        );
+    }
+
+    public function role(): BelongsTo
+    {
+        return $this->belongsTo(Role::class,'role_id');
     }
 }
