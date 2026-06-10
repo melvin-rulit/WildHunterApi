4467c455a100514896b6eae92fde4555ccb380a9
melvin <melvin.melvin.rulit@gmail.com>
2026-06-10 16:00:23 +0300

    файл перевода оружия

---
+++ src/resources/lang/ru/weapon.php
@@ -0,0 +1,31 @@
+<?php
+return [
+    'name' => [
+        'model_name' => 'Оружие',
+    ],
+    'errors' => [
+
+    ],
+    'rules' => [
+
+    ],
+    'validation' => [
+        'hunter_billet_number_required' => 'Номер билета обязателен',
+        'hunter_billet_number_string' => 'Номер билета должен быть строкой',
+
+        'hunter_license_number_required' => 'Номер лицензии обязателен',
+        'hunter_license_number_string' => 'Номер лицензии должен быть строкой',
+
+        'hunter_license_date_required' => 'Дата лицензии обязательна',
+        'hunter_license_date_invalid' => 'Неверная дата лицензии',
+
+        'weapon_type_required' => 'Тип оружия обязателен',
+        'weapon_type_not_found' => 'Тип оружия не найден',
+
+        'caliber_required' => 'Калибр обязателен',
+        'caliber_string' => 'Калибр должен быть строкой',
+    ],
+    'successes' => [
+        'save_success' => 'Оружие сохранено.',
+    ]
+];
