9f7e310537ef3af35b65f80b34b77996be1b599f
melvin <melvin.melvin.rulit@gmail.com>
2026-06-16 11:14:49 +0300

    добавил weapon_type_id в вывод оружий пользователя

--- src/modules/User/Http/Resources/UserWeaponResource.php
+++ src/modules/User/Http/Resources/UserWeaponResource.php
@@ -12,6 +12,7 @@ class UserWeaponResource extends BaseJsonResource
             'hunter_license_number' => $this->resource->hunter_license_number,
             'hunter_license_date' => $this->resource->hunter_license_date?->translatedFormat('d F Y г.'),
             'weapon_type' => $this->resource->type?->title,
+            'weapon_type_id' => $this->resource->weapon_type_id,
             'caliber' => $this->resource->caliber?->title,
         ];
     }
