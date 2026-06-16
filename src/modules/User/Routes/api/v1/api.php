0650379a7891b2e62ab2f9bd1294696f4d7fb61e
melvin <melvin.melvin.rulit@gmail.com>
2026-06-16 10:07:27 +0300

    подписка даже не для афторизованных

--- src/modules/User/Routes/api/v1/api.php
+++ src/modules/User/Routes/api/v1/api.php
@@ -8,14 +8,14 @@ use Modules\User\Controllers\Api\PasswordController;
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 
+//Newsletter
+Route::post('/user/newsletter/subscribe',[UserController::class, 'subscribe']);
+
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/users', [UserController::class, 'searchUsers']);
     Route::get('/user/{user}', [UserController::class, 'searchUser']);
     Route::post('/user', [UserController::class, 'profileUpdate']);
-
-    //Newsletter
-    Route::post('/user/newsletter/subscribe',[UserController::class, 'subscribe']);
 
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::post('/user/change-password', [PasswordController::class, 'updatePassword']);
