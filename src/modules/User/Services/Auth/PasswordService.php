2537ac6a40c67be65cbcf8751dc19c84c203d54f
melvin <melvin.melvin.rulit@gmail.com>
2026-06-17 10:53:12 +0300

    методы для сброса пароля

--- src/modules/User/Services/Auth/PasswordService.php
+++ src/modules/User/Services/Auth/PasswordService.php
@@ -2,12 +2,16 @@
 
 namespace Modules\User\Services\Auth;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Hash;
+use App\Exceptions\BusinessException;
 use Illuminate\Support\Facades\Crypt;
 use App\Exceptions\ValidationException;
 use Modules\User\Dto\Auth\UpdatePasswordData;
 use Illuminate\Contracts\Auth\Authenticatable;
+use Random\RandomException;
 
 class PasswordService
 {
@@ -37,6 +41,68 @@ class PasswordService
         return [
             'code' => 'password_update_successfully',
             'data' => $user
+        ];
+    }
+
+    /**
+     * @throws RandomException
+     */
+    public function sendResetCode($email): array
+    {
+        $ttl = (int) config('auth.password_reset.ttl');
+
+        $code = (string) random_int(100000, 999999);
+
+        $key = "reset-password:{$email}";
+
+        Cache::put($key, $code, now()->addMinutes($ttl));
+
+        return [
+            'code' => 'code_send_successfully',
+            'reset_code' => $code,
+            'ttl' => $ttl,
+        ];
+    }
+
+    /**
+     * @throws BusinessException
+     */
+    public function resetCode($email, $code, $password): array
+    {
+        $key = "reset_password:{$email}";
+
+        $cachedCode = Cache::get($key);
+
+        if (!$cachedCode) {
+            throw new BusinessException(
+                errorCode: 'code_expired',
+                domain: 'password'
+            );
+        }
+
+        if ($cachedCode !== $code) {
+            throw new BusinessException(
+                errorCode: 'code_invalid',
+                domain: 'password'
+            );
+        }
+
+        Cache::forget($key);
+
+        $user = User::where('email', $email)->first();
+
+        $user->tokens()->delete();
+
+        $user->password = Hash::make($password);
+        $user->current_password = Crypt::encryptString($password);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        $user->save();
+
+        return [
+            'code' => 'code_reset_successfully',
+            'token' => $token,
         ];
     }
 }
