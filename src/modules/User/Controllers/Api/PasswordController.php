a4681541ec0ac5a9da531700918a6081c5f0fd8d
melvin <melvin.melvin.rulit@gmail.com>
2026-06-17 10:52:37 +0300

    методы для сброса пароля

--- src/modules/User/Controllers/Api/PasswordController.php
+++ src/modules/User/Controllers/Api/PasswordController.php
@@ -2,19 +2,25 @@
 
 namespace Modules\User\Controllers\Api;
 
+use App\Exceptions\BusinessException;
+use Random\RandomException;
+use App\Service\MailService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
 use App\Exceptions\ValidationException;
 use App\Http\Responses\SuccessResponse;
 use Modules\User\Dto\Auth\UpdatePasswordData;
+use Modules\User\Emails\PasswordResetMessageEmail;
 use Modules\User\Events\PasswordUpdatedEvent;
 use Modules\User\Services\Auth\PasswordService;
+use Modules\User\Http\Requests\ResetPasswordRequest;
 use Modules\User\Http\Requests\ChangePasswordRequest;
+use Modules\User\Http\Requests\SendCodeResetPasswordRequest;
 
 
 class PasswordController
 {
-    public function __construct(private PasswordService $passwordService)
+    public function __construct(private PasswordService $passwordService, protected MailService $mailService,)
     {
     }
 
@@ -30,5 +36,30 @@ class PasswordController
         event(new PasswordUpdatedEvent($result['data']));
 
         return new SuccessResponse(code: $result['code'], domain: 'password');
+    }
+
+    /**
+     * @throws RandomException
+     */
+    public function sendResetCode(SendCodeResetPasswordRequest $request): JsonResponse
+    {
+        $result = $this->passwordService->sendResetCode($request->validated('email'));
+
+        $this->mailService->send( $request->input('email'), new PasswordResetMessageEmail($result['reset_code'], $result['ttl']) );
+
+        return new SuccessResponse(code: $result['code'], domain: 'password', replace: [
+            'minutes' => $result['ttl'],
+        ]);
+    }
+
+    /**
+     * @throws BusinessException
+     */
+    public function resetPassword(ResetPasswordRequest $request): JsonResponse
+    {
+        $data = $request->validated();
+        $result = $this->passwordService->resetCode( $data['email'], $data['code'], $data['password'] );
+
+        return new SuccessResponse(code: $result['code'], domain: 'password', data: $result['token']);
     }
 }
