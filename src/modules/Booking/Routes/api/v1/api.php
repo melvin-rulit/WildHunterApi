f3b43e982a25a4c28354320ac12d78606519399e
melvin <melvin.melvin.rulit@gmail.com>
2026-06-18 14:35:53 +0300

    маршрут получения броней пользователя

--- src/modules/Booking/Routes/api/v1/api.php
+++ src/modules/Booking/Routes/api/v1/api.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Modules\Booking\Controllers\BookingController;
 
 Route::middleware('auth:sanctum')->group(function () {
-
+    Route::get('/users/{id}/bookings', [BookingController::class, 'getWeapons']);
  });
