a32c43fbdb9fb50d069417797e528fd5cf5e3aae
melvin <melvin.melvin.rulit@gmail.com>
2026-06-19 08:23:35 +0300

    описал два метода получение коментариев и добавление в избранное

--- src/app/Swagger/Paths/HotelsPath.php
+++ src/app/Swagger/Paths/HotelsPath.php
@@ -56,4 +56,98 @@ class HotelsPath
     )]
     public function offers(): void
     {}
+
+    #[OA\Get(
+        path: "/api/" . ApiConfig::VERSION . "/hotels/{hotel_id}/comments",
+        summary: "Получить комментарии отеля",
+        security: [['bearerAuth' => []]],
+        tags: ["Hotels"],
+        parameters: [
+            new OA\Parameter(
+                name: "hotel_id",
+                in: "path",
+                required: true,
+                schema: new OA\Schema(
+                    type: "integer",
+                    example: 1
+                )
+            ),
+            new OA\Property(
+                property: "order_by",
+                type: "string",
+                example: "created_at"
+            ),
+            new OA\Property(
+                property: "order_direction",
+                type: "string",
+                example: "desc",
+                enum: ["asc", "desc"]
+            ),
+            new OA\Property(
+                property: "limit",
+                type: "integer",
+                example: "3"
+            ),
+        ],
+        responses: [
+            new OA\Response(
+                ref: "#/components/responses/SuccessResponse",
+                response: 200
+            ),
+            new OA\Response(
+                ref: "#/components/responses/ValidationError",
+                response: 422
+            ),
+            new OA\Response(
+                response: 401,
+                description: "Не авторизован",
+                content: new OA\JsonContent(
+                    properties: [
+                        new OA\Property(property: "message", type: "string", example: "Не авторизован")
+                    ]
+                )
+            )
+        ]
+    )]
+    public function comments(): void
+    {}
+
+    #[OA\Post(
+        path: "/api/" . ApiConfig::VERSION . "/hotels/{hotel_id}/favorite",
+        summary: "Добавить отель в избранное",
+        security: [['bearerAuth' => []]],
+        tags: ["Hotels"],
+        parameters: [
+            new OA\Parameter(
+                name: "hotel_id",
+                in: "path",
+                required: true,
+                schema: new OA\Schema(
+                    type: "integer",
+                    example: 1
+                )
+            ),
+        ],
+        responses: [
+            new OA\Response(
+                ref: "#/components/responses/SuccessResponse",
+                response: 200
+            ),
+            new OA\Response(
+                ref: "#/components/responses/ValidationError",
+                response: 422
+            ),
+            new OA\Response(
+                response: 401,
+                description: "Не авторизован",
+                content: new OA\JsonContent(
+                    properties: [
+                        new OA\Property(property: "message", type: "string", example: "Не авторизован")
+                    ]
+                )
+            )
+        ]
+    )]
+    public function addFavorite(): void
+    {}
 }
