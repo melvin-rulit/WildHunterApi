3dcbcefa58b29f5d5647544066f9b4353aafa815
melvin <melvin.melvin.rulit@gmail.com>
2026-06-22 19:52:19 +0300

    поменял вывод ресурса на ресурс с оружием

--- src/modules/User/Controllers/Api/UserController.php
+++ src/modules/User/Controllers/Api/UserController.php
@@ -5,6 +5,7 @@ namespace Modules\User\Controllers\Api;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
 use Modules\User\Dto\SubscribeData;
+use Modules\User\Http\Resources\UserWithWeaponResource;
 use Modules\User\Services\UserService;
 use App\Http\Responses\SuccessResponse;
 use Modules\User\Dto\ProfileUpdateData;
@@ -28,7 +29,7 @@ class UserController
     {
         $result = $this->userService->searchById($id);
 
-        return new SuccessResponse(data: new UserResource($result));
+        return new SuccessResponse(data: new UserWithWeaponResource($result));
     }
 
     public function profileUpdate(ProfileUpdateRequest $request): JsonResponse
@@ -36,7 +37,7 @@ class UserController
         $dto = ProfileUpdateData::fromRequest($request);
         $result = $this->userService->update(Auth::user(), $dto);
 
-        return new SuccessResponse(code: $result['code'], domain: 'user', data: new UserResource($result['user']));
+        return new SuccessResponse(code: $result['code'], domain: 'user', data: new UserWithWeaponResource($result['user']));
     }
 
     public function subscribe(SubscribeRequest $request): JsonResponse
