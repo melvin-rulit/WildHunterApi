0f89cb1b30a6e2364efada06d504b53d5662abeb
melvin <melvin.melvin.rulit@gmail.com>
2026-06-22 20:15:08 +0300

    добавил стиль вывода ошибки при не 401

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -3,6 +3,7 @@
 use App\Exceptions\BaseException;
 use App\Http\Middleware\AttachTraceId;
 use Illuminate\Foundation\Application;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -22,6 +23,16 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->render(function (\Throwable $e, $request) {
+
+            if ($e instanceof AuthenticationException) {
+                return response()->json([
+                    'success' => false,
+                    'message' => __('auth.errors.auth_token_error'),
+                    'error_code' => 'auth_token_error',
+                    'errors' => [],
+                    'trace_id' => $request->attributes->get('trace_id')
+                ], 401);
+            }
 
             if ($e instanceof ValidationException) {
                 return response()->json([
