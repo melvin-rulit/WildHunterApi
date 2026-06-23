40551049c8de0a866d8e016f1b10ffbfa7e612bb
melvin <melvin.melvin.rulit@gmail.com>
2026-06-23 08:27:18 +0300

    файл перевода для избранного

---
+++ src/resources/lang/ru/wishlist.php
@@ -0,0 +1,18 @@
+<?php
+return [
+    'name' => [
+        'model_name' => 'Оружие',
+    ],
+    'errors' => [
+        'already_favorite' => 'Уже добавлено в избранное',
+    ],
+    'rules' => [
+
+    ],
+    'validation' => [
+
+    ],
+    'successes' => [
+        'save_success' => 'Добавлено в избранное.',
+    ]
+];
