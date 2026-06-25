3156393c3bb8bbdad6b234cddb077c38066a62e8
melvin <melvin.melvin.rulit@gmail.com>
2026-06-25 10:58:42 +0300

    метод получения локаций

--- src/modules/Location/Routes/api/v1/api.php
+++ src/modules/Location/Routes/api/v1/api.php
@@ -3,7 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Location\Controllers\LocationController;
 
-Route::post('/locations/offers', [LocationController::class, 'getLocations']);
+Route::post('/locations/offers', [LocationController::class, 'getBestLocations']);
+Route::get('/locations', [LocationController::class, 'getLocations']);
 
 Route::middleware('auth:sanctum')->group(function () {
 
