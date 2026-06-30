9cb339624c923890f8f3ab5b2c49c334c7d895c6
melvin <melvin.melvin.rulit@gmail.com>
2026-06-30 10:37:34 +0300

    перевод для отелей

--- src/resources/lang/ru/hotel.php
+++ src/resources/lang/ru/hotel.php
@@ -4,7 +4,7 @@ return [
         'model_name' => 'Отели',
     ],
     'errors' => [
-
+        'hotel_not_found' => 'Отель не найден',
     ],
     'rules' => [
 
@@ -17,6 +17,22 @@ return [
 
         'limit_must_be_numeric' => 'Параметр limit должен быть числом',
         'limit_min_value' => 'Минимальное значение limit — 1',
+
+        'location_id_must_be_integer' => 'Поле "локация" должно быть числом',
+        'animal_id_must_be_integer' => 'Поле "животные" должно быть числом',
+
+        'check_in_required' => 'Укажите дату заезда',
+        'check_in_must_be_date' => 'Дата заезда должна быть корректной датой',
+
+        'check_out_required' => 'Укажите дату выезда',
+        'check_out_must_be_date' => 'Дата выезда должна быть корректной датой',
+        'check_out_must_be_after_check_in' => 'Дата выезда должна быть позже даты заезда',
+
+        'adults_must_be_integer' => 'Количество взрослых должно быть числом',
+        'adults_min_value' => 'Минимальное количество взрослых — 1',
+
+        'children_must_be_integer' => 'Количество детей должно быть числом',
+        'children_min_value' => 'Минимальное количество детей — 0',
     ],
     'successes' => [
 
